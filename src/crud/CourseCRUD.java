abffd1cb8e02af7b79ea3c727abc4441eff01d3b
deepak2136 <deepakprajapati589@gmail.com>
2017-10-24 21:41:10 +0530

    ActionRedirect file updated

--- src/crud/CourseCRUD.java
+++ src/crud/CourseCRUD.java
@@ -16,45 +16,44 @@ import util.Response;
 
 public class CourseCRUD extends CRUDCore {
 
-	@Override
-	public Integer create(HttpServletRequest request) {
-		
-		Integer id = null;
-		
-		try {
-//			tx = session.beginTransaction();
-			String course_name = request.getParameter("course_name");
-			String course_id = request.getParameter("course_id");
-			Integer course_credits = Integer.parseInt(request.getParameter("course_credits"));
-			CoursePOJO course_obj = new CoursePOJO(course_name, course_id, course_credits);
-			id = (Integer) session.save(course_obj);
-			System.out.println(id);
-			tx.commit();
-		} catch (HibernateException e) {
-			if (tx != null)
-				tx.rollback();
-			e.printStackTrace();
-		} finally {
-			session.close();
-		}
-		return id;
-	}
+    @Override
+    public Integer create(HttpServletRequest request) {
 
-	@Override
-	public Object retrive(HttpServletRequest request) {
-		Response response = null;
-		try {
-			List<CoursePOJO> courses = session.createQuery("FROM CoursePOJO").list();
-			response = GeneralUtility.generateSuccessResponse(null, courses);
-		} catch (HibernateException e) {
-			tx.rollback();
-			e.printStackTrace();
-		} finally {
-			session.close();
-		}
-		return response.toString();
-	}
-	
-	
+        Integer id = null;
+
+        try {
+//			tx = session.beginTransaction();
+            String course_name = request.getParameter("course_name");
+            String course_id = request.getParameter("course_id");
+            Integer course_credits = Integer.parseInt(request.getParameter("course_credits"));
+            CoursePOJO course_obj = new CoursePOJO(course_name, course_id, course_credits);
+            id = (Integer) session.save(course_obj);
+            System.out.println(id);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return id;
+    }
+
+    @Override
+    public Object retrive(HttpServletRequest request) {
+        Response response = null;
+        try {
+            List<CoursePOJO> courses = session.createQuery("FROM CoursePOJO").list();
+            response = GeneralUtility.generateSuccessResponse(null, courses);
+        } catch (HibernateException e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return response.toString();
+    }
 
 }
--- src/util/GeneralUtility.java
+++ src/util/GeneralUtility.java
@@ -15,87 +15,88 @@ import com.google.gson.JsonParser;
 
 public final class GeneralUtility {
 
-	private static GeneralUtility instance;
-	private static Gson json;
-	private final static String ACTION_REDIRECT = "controller/ActionRedirect.properties";
+    private static GeneralUtility instance;
+    private static Gson json;
+    private final static String ACTION_REDIRECT = "controller/ActionRedirect.properties";
 
-	private GeneralUtility() {
-	}
+    private GeneralUtility() {
+    }
 
-	private static GeneralUtility getInstance() {
-		if (instance == null) {
-			instance = new GeneralUtility();
-			json = new Gson();
-		}
-		return instance;
-	}
+    private static GeneralUtility getInstance() {
+        if (instance == null) {
+            instance = new GeneralUtility();
+            json = new Gson();
+        }
+        return instance;
+    }
 
-	private String readAuthJSONUtil() {
-		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("auth.json");
-		StringBuffer data = new StringBuffer();
-		int item;
-		try {
-			while ((item = stream.read()) != -1) {
-				data.append((char) item);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return data.toString();
-	}
+    private String readAuthJSONUtil() {
+        InputStream stream = this.getClass().getClassLoader().getResourceAsStream("auth.json");
+        StringBuffer data = new StringBuffer();
+        int item;
+        try {
+            while ((item = stream.read()) != -1) {
+                data.append((char) item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data.toString();
+    }
 
-	public final static String readAuthJSON() {
-		String data = getInstance().readAuthJSONUtil();
-		return data;
-	}
+    public final static String readAuthJSON() {
+        String data = getInstance().readAuthJSONUtil();
+        return data;
+    }
 
-	public static Response generateUnauthorizedResponse() {
-		Response response = new Response("401", "Unauthorized", null, null);
-		return response;
-	}
+    public static Response generateUnauthorizedResponse() {
+        Response response = new Response("401", "Unauthorized", null, null);
+        return response;
+    }
 
-	public static Response generateSuccessResponse(String redirect, Object data) {
-		Response response = new Response(redirect, data);
-		response.setStatusCode("200");
-		response.setMessage("Action Successful");
-		return response;
-	}
+    public static Response generateSuccessResponse(String redirect, Object data) {
+        Response response = new Response(redirect, data);
+        response.setStatusCode("200");
+        response.setMessage("Action Successful");
+        return response;
+    }
 
-	public static boolean isAutheticatedUser(HttpServletRequest request) {
-		boolean authenticated = false;
-		HttpSession session = request.getSession();
-		String category;
-		if ((category = (String) session.getAttribute("userCategory")) != null) {
-			String action = request.getParameter("action");
-			if (action != null) {
-				JsonParser parser = new JsonParser();
-				JsonArray roles = parser.parse(readAuthJSON()).getAsJsonArray();
-				outer: for (JsonElement ele : roles) {
-					UserRole role = json.fromJson(ele, UserRole.class);
-					if (role.getRole().equalsIgnoreCase(category)) {
-						Map<String, String> allowedAction = role.getAction();
-						for (String key : allowedAction.keySet()) {
-							if (key.equalsIgnoreCase(action)) {
-								authenticated = true;
-								break outer;
-							}
-						}
-					}
-				}
-			}
-		}
-		return authenticated;
-	}
+    public static boolean isAutheticatedUser(HttpServletRequest request) {
+        boolean authenticated = false;
+        HttpSession session = request.getSession();
+        String category;
+        if ((category = (String) session.getAttribute("userCategory")) != null) {
+            String action = request.getParameter("action");
+            if (action != null) {
+                JsonParser parser = new JsonParser();
+                JsonArray roles = parser.parse(readAuthJSON()).getAsJsonArray();
+                outer:
+                for (JsonElement ele : roles) {
+                    UserRole role = json.fromJson(ele, UserRole.class);
+                    if (role.getRole().equalsIgnoreCase(category)) {
+                        Map<String, String> allowedAction = role.getAction();
+                        for (String key : allowedAction.keySet()) {
+                            if (key.equalsIgnoreCase(action)) {
+                                authenticated = true;
+                                break outer;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return authenticated;
+    }
 
-	public static String getRedirect(HttpServletRequest request) throws IOException {
-		String mode = request.getParameter("mode");
-		if (mode == null || mode.equalsIgnoreCase("api")) {
-			return null;
-		}
-		String theAction = request.getParameter("action");
-		Properties map = new Properties();
-		map.load(instance.getClass().getClassLoader().getResourceAsStream(ACTION_REDIRECT));
-		String action_redirect = map.getProperty(theAction.toLowerCase());
-		return action_redirect;
-	}
+    public static String getRedirect(HttpServletRequest request) throws IOException {
+        String mode = request.getParameter("mode");
+        if (mode == null || mode.equalsIgnoreCase("api")) {
+            return null;
+        }
+        String theAction = request.getParameter("action");
+        Properties map = new Properties();
+        map.load(instance.getClass().getClassLoader().getResourceAsStream(ACTION_REDIRECT));
+        String action_redirect = map.getProperty(theAction.toLowerCase());
+        return action_redirect;
+    }
 }
