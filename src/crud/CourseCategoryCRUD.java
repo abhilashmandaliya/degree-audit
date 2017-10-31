04cdb2300fea1674445e5ea8a1b7b707f069ce83
savan0714 <savan0014@gmail.com>
2017-10-31 11:58:46 +0530

    Course category added

---
+++ src/crud/CourseCategoryCRUD.java
@@ -0,0 +1,65 @@
+package crud;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.HibernateException;
+
+import pojo.CourseCategoryPOJO;
+import pojo.ProgramPOJO;
+import util.GeneralUtility;
+import util.Response;
+
+public class CourseCategoryCRUD extends CRUDCore {
+
+	@Override
+	public Response create(HttpServletRequest request) throws IOException {
+		Integer id = null;
+
+		try {
+			String course_cat_name = request.getParameter("course_cat_name");
+			String course_cat_code = request.getParameter("course_cat_code");
+			CourseCategoryPOJO courseCat = new CourseCategoryPOJO(course_cat_name, course_cat_code);
+			id = (Integer) session.save(courseCat);
+			tx.commit();
+			response = GeneralUtility.generateSuccessResponse(GeneralUtility.getRedirect(request), id);
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return response;
+	}
+
+	@Override
+	public Object retrive(HttpServletRequest request) throws IOException {
+		Response response = null;
+		try {
+			List<CourseCategoryPOJO> course_cats = session.createQuery("FROM CourseCategoryPOJO").list();
+			response = GeneralUtility.generateSuccessResponse(null, course_cats);
+		} catch (HibernateException e) {
+			tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return response.toString();
+	}
+
+	@Override
+	public Response update(HttpServletRequest request) throws IOException {
+
+		return null;
+	}
+
+	@Override
+	public Response delete(HttpServletRequest request) throws IOException {
+
+		return null;
+	}
+
+}
