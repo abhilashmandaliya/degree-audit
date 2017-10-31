8f30aef7348cdcb7fb39dc54dff217d6643a2303
Dipak Prajapati <deepakprajapati589@gmail.com>
2017-10-31 17:27:31 +0530

    grade_card updated

--- src/pojo/StudentPOJO.java
+++ src/pojo/StudentPOJO.java
@@ -2,66 +2,74 @@ package pojo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "student")
 public class StudentPOJO {
 
-    @Id
-    @Column(name = "student_id")
-    private long student_id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int student_id;
 
-    @Column(name = "student_name")
-    private String student_name;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private UserPOJO user_id;
 
-    @Column(name = "program_id")
-    private int program_id;
+	@ManyToOne
+	@JoinColumn(name = "program_id")
+	private ProgramPOJO program_id;
 
-    @Column(name = "year_of_enrolment")
-    private int year_of_enrolment;
+	@Column(name = "year_of_enrolment")
+	private int year_of_enrolment;
 
-    public StudentPOJO() {
-    }
+	public StudentPOJO() {
+	}
 
-    public StudentPOJO(long student_id, String student_name, int program_id, int year_of_enrolment) {
-        this.student_id = student_id;
-        this.student_name = student_name;
-        this.program_id = program_id;
-        this.year_of_enrolment = year_of_enrolment;
-    }
+	public StudentPOJO(UserPOJO user_id, ProgramPOJO program_id, int year_of_enrolment) {
+		this.user_id = user_id;
+		this.program_id = program_id;
+		this.year_of_enrolment = year_of_enrolment;
+	}
 
-    public long getStudent_id() {
-        return student_id;
-    }
+	public int getStudent_id() {
+		return student_id;
+	}
 
-    public void setStudent_id(long student_id) {
-        this.student_id = student_id;
-    }
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
 
-    public String getStudent_name() {
-        return student_name;
-    }
+	public UserPOJO getUser_id() {
+		return user_id;
+	}
 
-    public void setStudent_name(String student_name) {
-        this.student_name = student_name;
-    }
+	public void setUser_id(UserPOJO user_id) {
+		this.user_id = user_id;
+	}
 
-    public int getProgram_id() {
-        return program_id;
-    }
+	public ProgramPOJO getProgram_id() {
+		return program_id;
+	}
 
-    public void setProgram_id(int program_id) {
-        this.program_id = program_id;
-    }
+	public void setProgram_id(ProgramPOJO program_id) {
+		this.program_id = program_id;
+	}
 
-    public int getYear_of_enrolment() {
-        return year_of_enrolment;
-    }
+	public int getYear_of_enrolment() {
+		return year_of_enrolment;
+	}
 
-    public void setYear_of_enrolment(int year_of_enrolment) {
-        this.year_of_enrolment = year_of_enrolment;
-    }
+	public void setYear_of_enrolment(int year_of_enrolment) {
+		this.year_of_enrolment = year_of_enrolment;
+	}
 
 }
